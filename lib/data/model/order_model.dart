46d7027371b956124bfe3e12afc66cacd5b34864
AMOSAD0 <mossadahmed975@gmail.com>
2022-04-06 00:11:02 +0200

    .

--- lib/data/model/order_model.dart
+++ lib/data/model/order_model.dart
@@ -4,12 +4,12 @@ import 'package:app02/data/model/product_Model.dart';
 
 class Order{
 
-  Map <String,dynamic>?maptOfNames;
+  Map <String,dynamic>maptOfNames=new Map();
   //List ? listOfName; 
 
 convrt(List<Product> list){
-  for(int i=0;i<=list.length;i++){
-    maptOfNames!['Name $i']=list[i].Name;
+  for(int i=1;i<=list.length;i++){
+    maptOfNames['Name $i']=list[i].Name;
   }
   return jsonEncode(maptOfNames);
 }
